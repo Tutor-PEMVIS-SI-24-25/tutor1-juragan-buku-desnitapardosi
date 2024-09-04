aacb7e39121b3daa6c5e32a1c3a28c38c77cad99
desnitapardosi <desnitapardosi970@gmail.com>
2024-09-04 21:27:16 +0700

    Telah menyelesaikan tugas

--- T01.java
+++ T01.java
@@ -1,3 +1,17 @@
-// NIM - NAMA
-// NIM - NAMA
+// 12S24043 - desnita pardosi
+// 12S24043 - desnita pardosi
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String nama;
+
+        nama = input.nextLine();
+        System.out.println(nama);
+    }
+}
+
 
